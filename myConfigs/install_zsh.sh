128a1c821b59249a9128c0d6354aeb606627bb24
touEr_ <61647484+weiensong@users.noreply.github.com>
2023-10-27 10:34:40 +0800

    feat: zsh install and config

---
+++ myConfigs/install_zsh.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+# author: weiensong
+# email: touer0018@gmail.com
+# date: 2023.10.27
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install zsh -y
+
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting &&
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions &&
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+
+chsh -s /bin/zsh
+
+git config --global user.name "weiensong"
+git config --global user.email "touer0018@gmail.com"
