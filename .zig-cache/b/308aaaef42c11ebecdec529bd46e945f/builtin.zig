573050fa8537aa169c69ffef63e6065b37b91349
jstrieb/github-stats <github-stats[bot]@jstrieb.github.io>
2026-04-20 03:09:48 +0000

    Update generated files

---
+++ .zig-cache/b/308aaaef42c11ebecdec529bd46e945f/builtin.zig
@@ -0,0 +1,143 @@
+const std = @import("std");
+/// Zig version. When writing code that supports multiple versions of Zig, prefer
+/// feature detection (i.e. with `@hasDecl` or `@hasField`) over version checks.
+pub const zig_version = std.SemanticVersion.parse(zig_version_string) catch unreachable;
+pub const zig_version_string = "0.15.2";
+pub const zig_backend = std.builtin.CompilerBackend.stage2_llvm;
+
+pub const output_mode: std.builtin.OutputMode = .Exe;
+pub const link_mode: std.builtin.LinkMode = .static;
+pub const unwind_tables: std.builtin.UnwindTables = .async;
+pub const is_test = false;
+pub const single_threaded = false;
+pub const abi: std.Target.Abi = .gnu;
+pub const cpu: std.Target.Cpu = .{
+    .arch = .x86_64,
+    .model = &std.Target.x86.cpu.znver4,
+    .features = std.Target.x86.featureSet(&.{
+        .@"64bit",
+        .adx,
+        .aes,
+        .allow_light_256_bit,
+        .avx,
+        .avx2,
+        .avx512bf16,
+        .avx512bitalg,
+        .avx512bw,
+        .avx512cd,
+        .avx512dq,
+        .avx512f,
+        .avx512ifma,
+        .avx512vbmi,
+        .avx512vbmi2,
+        .avx512vl,
+        .avx512vnni,
+        .avx512vpopcntdq,
+        .bmi,
+        .bmi2,
+        .branchfusion,
+        .clflushopt,
+        .clwb,
+        .clzero,
+        .cmov,
+        .crc32,
+        .cx16,
+        .cx8,
+        .evex512,
+        .f16c,
+        .fast_15bytenop,
+        .fast_bextr,
+        .fast_dpwssd,
+        .fast_imm16,
+        .fast_lzcnt,
+        .fast_movbe,
+        .fast_scalar_fsqrt,
+        .fast_scalar_shift_masks,
+        .fast_variable_perlane_shuffle,
+        .fast_vector_fsqrt,
+        .fma,
+        .fsgsbase,
+        .fsrm,
+        .fxsr,
+        .gfni,
+        .idivq_to_divl,
+        .invpcid,
+        .lzcnt,
+        .macrofusion,
+        .mmx,
+        .movbe,
+        .nopl,
+        .pclmul,
+        .popcnt,
+        .prfchw,
+        .rdpid,
+        .rdpru,
+        .rdrnd,
+        .rdseed,
+        .sahf,
+        .sbb_dep_breaking,
+        .sha,
+        .shstk,
+        .slow_shld,
+        .smap,
+        .smep,
+        .sse,
+        .sse2,
+        .sse3,
+        .sse4_1,
+        .sse4_2,
+        .sse4a,
+        .ssse3,
+        .vaes,
+        .vpclmulqdq,
+        .vzeroupper,
+        .x87,
+        .xsave,
+        .xsavec,
+        .xsaveopt,
+        .xsaves,
+    }),
+};
+pub const os: std.Target.Os = .{
+    .tag = .linux,
+    .version_range = .{ .linux = .{
+        .range = .{
+            .min = .{
+                .major = 6,
+                .minor = 17,
+                .patch = 0,
+            },
+            .max = .{
+                .major = 6,
+                .minor = 17,
+                .patch = 0,
+            },
+        },
+        .glibc = .{
+            .major = 2,
+            .minor = 39,
+            .patch = 0,
+        },
+        .android = 29,
+    }},
+};
+pub const target: std.Target = .{
+    .cpu = cpu,
+    .os = os,
+    .abi = abi,
+    .ofmt = object_format,
+    .dynamic_linker = .init("/lib64/ld-linux-x86-64.so.2"),
+};
+pub const object_format: std.Target.ObjectFormat = .elf;
+pub const mode: std.builtin.OptimizeMode = .ReleaseSafe;
+pub const link_libc = false;
+pub const link_libcpp = false;
+pub const have_error_return_tracing = false;
+pub const valgrind_support = false;
+pub const sanitize_thread = false;
+pub const fuzz = false;
+pub const position_independent_code = false;
+pub const position_independent_executable = false;
+pub const strip_debug_info = false;
+pub const code_model: std.builtin.CodeModel = .default;
+pub const omit_frame_pointer = false;
